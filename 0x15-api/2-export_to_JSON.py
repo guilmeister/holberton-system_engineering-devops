63c995d48cd6bb48b1f5bd435f7b66954bbf2626
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-06 02:56:15 +0000

    added task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that, using this REST API, for a given employee ID, returns information
+about his/her TODO list progress.
+"""
+
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = requests.get(url + 'users/{}'.format(sys.argv[1]))
+    name = users.json().get('username')
+    todos = requests.get(url + 'todos')
+    todo = todos.json()
+    completed_tasks = 0
+    total_tasks = 0
+    new_dict = {sys.argv[1]: []}
+
+    for t in todo:
+        if t.get('userId') == int(sys.argv[1]):
+            new_dict.get(sys.argv[1]).append({
+                "task": t.get('title'),
+                "completed": t.get('completed'),
+                "username": name
+                })
+
+    with open('{}.json'.format(sys.argv[1]), 'w') as data:
+        json.dump(new_dict, data)
