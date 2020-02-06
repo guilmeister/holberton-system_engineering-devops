f1292b8681f43e0af85119eed2e6c15e769293a1
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-06 01:32:52 +0000

    added if name == main condition

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,24 +8,25 @@ import requests
 import sys
 
 
-url = 'https://jsonplaceholder.typicode.com/'
-users = requests.get(url + 'users/{}'.format(sys.argv[1]))
-name = users.json().get('name')
-todos = requests.get(url + 'todos')
-todo = todos.json()
-completed_tasks = 0
-total_tasks = 0
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = requests.get(url + 'users/{}'.format(sys.argv[1]))
+    name = users.json().get('name')
+    todos = requests.get(url + 'todos')
+    todo = todos.json()
+    completed_tasks = 0
+    total_tasks = 0
 
-for t in todo:
-    if (t.get('userId') == int(sys.argv[1])):
-        total_tasks = total_tasks + 1
-        if (t.get('completed')):
-            completed_tasks = completed_tasks + 1
+    for t in todo:
+        if (t.get('userId') == int(sys.argv[1])):
+            total_tasks = total_tasks + 1
+            if (t.get('completed')):
+                completed_tasks = completed_tasks + 1
 
-print('Employee {} is done with tasks({}/{}):'.format(name,
-                                                      completed_tasks,
-                                                      total_tasks))
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+                                                          completed_tasks,
+                                                          total_tasks))
 
-for t in todo:
-    if (t.get('userId') == int(sys.argv[1]) and t.get('completed')):
-        print('     ' + t.get('title'))
+    for t in todo:
+        if (t.get('userId') == int(sys.argv[1]) and t.get('completed')):
+            print('     ' + t.get('title'))
