a0a2b1e80ee778aefda27856634a70912153679c
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-06 01:59:17 +0000

    added task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that, using this REST API, for a given employee ID, returns information
+about his/her TODO list progress.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = requests.get(url + 'users/{}'.format(sys.argv[1]))
+    name = users.json().get('name')
+    todos = requests.get(url + 'todos')
+    todo = todos.json()
+    completed_tasks = 0
+    total_tasks = 0
+
+    with open('{}.csv'.format(sys.argv[1]), 'w') as data:
+        for t in todo:
+            if (t.get('userId') == int(sys.argv[1])):
+                data.write('"{}","{}","{}","{}"\n'.format(t.get('userId'),
+                                                          name,
+                                                          t.get('completed'),
+                                                          t.get('title')))
